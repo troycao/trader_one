ccb2d20a0b82c34575af0a4113ee93016c502f34
caoqiang <caoqiang@zenmen.com>
2021-05-01 09:58:37 +0800

    test

--- com/troy/trader/model/trader.py
+++ com/troy/trader/model/trader.py
@@ -5,14 +5,32 @@ from tensorflow import keras
 from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import glob
 
 # 定义数据
-print(tf.__version__)
-print(tf.test.gpu_device_name)
+# print(tf.__version__)
 
 # data_path = glob.glob('G:/data/stock/20210418/*.txt')
 # print(data_path[:5])
 # data = np.loadtxt('G:/data/stock/20210418/SH#600000.txt')
-data = np.genfromtxt('../data/SH#600000.txt', dtype=None)
+# data = np.genfromtxt('../data/SH#600000.txt', dtype=None)
+# data = np.array(data)
+# print(data)
+
+data = pd.read_csv('../data/SH#600000.csv')
+print(type(data))
+# print(data[:, ['close']])
+print(data.head())
+print(data.index)
+print(data.columns)
+print(data['tradeDate'])
+print(data.loc[:, ['open','close']])
+print(data.iloc[3])
+print(data[data.close > 60])
+print(data.mean())
+upDown = data['open'].apply(lambda x: x>0)
+data['upDown'] = upDown
 print(data)
+# def getTraderSignal(data):
+#     print(data[0:3])
---
+++ com/troy/trader/test/ArrayTest.py
@@ -0,0 +1,60 @@
+# coding:utf-8
+
+# 一维数组
+# cars = ['奔驰','宝马','奥迪']
+# print(cars)
+# for car in cars:
+#     print(car)
+#
+# print("=====")
+# for i in range(len(cars)):
+#     print(cars[i])
+
+# print(cars.pop())
+# print(cars)
+# print(cars[0:])
+# list 有序 可更改 可重复
+# list = ['apple','orange','branna',1]
+# print(list)
+# for a in list:
+#     print(a)
+#
+# list.append('test')
+# print(list)
+# # print(list[0:-1])
+# # print(list[1:4])
+# list.remove(1)
+# print(list)
+#
+# list1 = list
+# print(list1)
+# list.append('test2')
+# print(list1)
+# list2 = list.copy()
+# list.append('test3')
+# print(list2)
+#
+# list4 = list + list1
+# print(list4)
+# list.extend(list1)
+# print(list)
+# list.sort()
+# print(list)
+# list.reverse()
+# print(list)
+
+# tuple 有序  不可更改
+# tuple = ('apple','orange','branna')
+# print(tuple.count('apple'))
+# print(tuple.index('apple'))
+# print(tuple[1:3])
+# x = ('apple','orange','branna')
+# y = list(x)
+
+# set 无序 可更改 不可重复 无索引
+set ={'apple','orange','branan','mango'}
+print(set)
+for furit in set:
+    print(furit)
+
+print()
