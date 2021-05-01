0f7c81170d9b8e71412efaaf7b207cfd96bee834
troy <caoq1987@gmail.com>
2021-05-01 10:43:33 +0800

    test

---
+++ com/troy/trader/practice/data_practice.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+import numpy as np
+
+# array = np.array([[1,2,3],[4,5,6]])
+# print(array)
+# print(array.shape)
+# print(array.ndim)
+# print(array.size)
+
+array1 = np.zeros((2,3))
+print(array1)
+
+array2 = np.linspace(1,10,10).reshape((2,5))
+print(array2)
+
+
+print(array2)
