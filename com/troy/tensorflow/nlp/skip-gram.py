f48ddacc44de5a79de4f00f71685c3d5ed74a893
troy <caoq1987@gmail.com>
2021-07-23 22:51:59 +0800

    npl

--- com/troy/tensorflow/nlp/skip-gram.py
+++ com/troy/tensorflow/nlp/skip-gram.py
@@ -29,38 +29,43 @@ corpus = [
     "i o h n 9 9 d 9 f a 9",
 ]
 
-
-class CBOW(keras.Model):
+class SkipGram(keras.Model):
     def __init__(self, v_dim, emb_dim):
         super().__init__()
         self.v_dim = v_dim
         self.embeddings = keras.layers.Embedding(
-            input_dim=v_dim, output_dim=emb_dim,  # [n_vocab, emb_dim]
-            embeddings_initializer=keras.initializers.RandomNormal(0., 0.1),
+            input_dim=v_dim,
+            output_dim=emb_dim,
+            embeddings_initializer=keras.initializers.RandomNormal(0, 0,1)
         )
 
-        # noise-contrastive estimation
-        self.nce_w = self.add_weight(
-            name="nce_w", shape=[v_dim, emb_dim],
-            initializer=keras.initializers.TruncatedNormal(0., 0.1))  # [n_vocab, emb_dim]
-        self.nce_b = self.add_weight(
-            name="nce_b", shape=(v_dim,),
-            initializer=keras.initializers.Constant(0.1))  # [n_vocab, ]
-
+        self.nec_w = self.add_weight(
+            name="nec_w",
+            shape=[v_dim, emb_dim],
+            initializer=keras.initializers.TruncatedNormal(0, 0.1)
+        )
+        self.nec_b = self.add_weight(
+            name="nec_b",
+            shape=(v_dim,),
+            initializer=keras.initializers.Constant(0.1)
+        )
         self.opt = keras.optimizers.Adam(0.01)
 
     def call(self, x, training=None, mask=None):
-        # x.shape = [n, skip_window*2]
-        o = self.embeddings(x)
-        o = tf.reduce_mean(o, axis=1)
-        return o
+        return self.embeddings(x)
 
     def loss(self, x, y, training=None):
         embedded = self.call(x, training)
         return tf.reduce_mean(
             tf.nn.nce_loss(
-                weights=self.nce_w, biases=self.nce_b, labels=tf.expand_dims(y, axis=1),
-                inputs=embedded, num_sampled=5, num_classes=self.v_dim))
+                weights=self.nec_w,
+                biases=self.nec_b,
+                labels=tf.expand_dims(y, axis=1),
+                inputs=embedded,
+                num_sampled=5,
+                num_classes=self.v_dim
+            )
+        )
 
     def step(self, x, y):
         with tf.GradientTape() as tape:
@@ -69,19 +74,16 @@ class CBOW(keras.Model):
         self.opt.apply_gradients(zip(grads, self.trainable_variables))
         return loss.numpy()
 
-
-def train(model, data):
-    for t in range(2500):
+def train(model,data):
+    for t in range(5000):
         bx, by = data.sample(8)
         loss = model.step(bx, by)
-        if t % 200 == 0:
-            print("step: {} | loss: {}".format(t, loss))
+        if t % 200 ==0:
+            print("step:{} | loss:{}".format(t, loss))
 
-
-if __name__ == "__main__":
-    d = process_w2v_data(corpus, skip_window=2, method="cbow")
-    m = CBOW(d.num_word, 2)
+if __name__ == '__main__':
+    d = process_w2v_data(corpus, skip_window=2, method="skip_gram")
+    m = SkipGram(d.num_word, 2)
     train(m, d)
 
-    # plotting
-    show_w2v_word_embedding(m, d, "/Users/troy/work/ai/trader_one/com/troy/data/result/cbow.png")
+    show_w2v_word_embedding(m, d, "/Users/troy/work/ai/trader_one/com/troy/data/result/skip-gram.png")
